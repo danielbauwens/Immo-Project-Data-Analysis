d5a3c215303a55e50b26910fe0459ee5b82ede86
danielbauwens <69080058+danielbauwens@users.noreply.github.com>
2023-07-05 11:09:52 +0200

    created notebook, added dataset

--- analysis.py
+++ analysis.py
@@ -1,2 +1,6 @@
 import seaborn as sns
 import pandas as pd
+
+ds = pd.read_csv('https://raw.githubusercontent.com/danielbauwens/challenge-collecting-data/main/property_data.csv')
+
+print(ds.head())
