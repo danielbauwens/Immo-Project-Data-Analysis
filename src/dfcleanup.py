31f4ddfd967df7059c6cba8e2d81189466b1d5b9
danielbauwens <69080058+danielbauwens@users.noreply.github.com>
2023-07-18 11:59:05 +0200

    building py source code for regression model

--- src/dfcleanup.py
+++ src/dfcleanup.py
@@ -2,7 +2,16 @@ import pandas as pd
 import numpy as np
 
 def cleaning(df):
-
+    # Filling NaN values with 0.
     df['landplot'].fillna(0, inplace=True)
     df['facades'].fillna(0, inplace=True)
     df['Living area'].fillna(0, inplace=True)
+
+    # Creating dummy columns from categorical data.
+    df = pd.get_dummies(df, columns=['condition', 'subtype', 'province', 'Zip code'])
+
+    # Removing features that we won't be using.
+    df.drop(['type', 'city', 'facades', 'Terrace', 'Kitchen'], axis=1, inplace=True)
+
+    # Because 'get_dummies()' creates boolean values, we re-define our dataframe to be integers only.
+    df = df.astype(int)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from src.dfcleanup import cleaning
-from src.training import 
+from src.training import model
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-# Reading our Immoweb dataset in 'df'.
+# Reading our Immoweb dataset in 'df' and calling the 'cleaning()' function to process it before training our model.
 df = cleaning(pd.read_csv('../data/merged_data.csv'))
 
 # Instantiating LinearRegression as 'reg'.
 reg = LinearRegression()
+
+# Calling our training/testing model
+model(df, reg, np)
